75f971516f521f00ee6075b64ac08f3869f1859e
NickCh1996 <foouli@hotmail.com>
2023-09-22 19:25:11 +0300

    modified .Build included project path and GAS modules, begun implementation of AttributeSetBase

---
+++ Source/StretchyUnderdog/GameInstances/ShishaGameInstnace.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "ShishaGameInstnace.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STRETCHYUNDERDOG_API UShishaGameInstnace : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
